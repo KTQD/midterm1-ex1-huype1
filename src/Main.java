35d99666ad1beb2d9f2aedf65ae6c7d3c8640279
Hoang Quoc Huy <119068593+huype1@users.noreply.github.com>
2024-03-14 07:58:48 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
+
 public class Main {
     public static void main(String[] args) {
-        MP3 mp3 = new MP3("CarSound", "ASMR", "1CRS", (float) 3.5, (float) 2.1);
-        Book book = new Book("Can't Hurt Me: Master Your Mind and Defy the Odds", "", "BSMT321", (float) 10.5, "David Goggin", 364, "Motivation" );
+        Item mp3 = new MP3("CarSound", "ASMR", "1CRS", (float) 3.5, (float) 2.1);
+        Item book = new Book("Can't Hurt Me: Master Your Mind and Defy the Odds", "", "BSMT321", (float) 10.5, "David Goggin", 364, "Motivation" );
         mp3.showInfo();
         book.showInfo();
 
