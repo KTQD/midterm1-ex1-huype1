de55dddabcb736dda3e8172e13dc3640d71a7746
Hoang Quoc Huy <119068593+huype1@users.noreply.github.com>
2024-03-14 07:53:48 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,13 @@
-public class MP3 {
+public class MP3 extends Item{
+    public float duration;
+
+    public MP3(String name, String description, String ID, float price, float duration) {
+        super(name, description, ID, price);
+        this.duration = duration;
+    }
+
+
+    public void showInfo() {
+        System.out.println("This is a sound data type name: " + name + " Description: " + getDescription() + " Duration: " + duration);
+    }
 }
