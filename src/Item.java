669b9ba2a775143cb307684402ead70b019f829b
Hoang Quoc Huy <119068593+huype1@users.noreply.github.com>
2024-03-14 07:52:34 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,25 @@
-public class Item {
+public abstract class Item {
+    public String name;
+
+    public java.lang.String getDescription() {
+        return description;
+    }
+
+    public void setDescription(java.lang.String description) {
+        this.description = description;
+    }
+
+    private String description;
+    protected String ID;
+    float price;
+
+    public Item(java.lang.String name, java.lang.String description, java.lang.String ID, float price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+
+    abstract void showInfo();
+
 }
